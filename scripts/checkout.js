0190c5926392fdc2bfbe399973cdb02acd65f2c9
Enugu Dhanush <dhanushenugu22052006@gmail.com>
2025-08-15 02:29:17 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import {renderOrderSummary} from './checkout/OrderSummary.js';  
+import {renderPaymentSummary} from './checkout/paymentSummary.js'
 
 renderOrderSummary();
+renderPaymentSummary();
 //To display cart items when the page first loads.
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,6 @@
 // Contains utility related to money
 export function formatCurrency(priceCents){
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
   
 }
 
