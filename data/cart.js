1a3fe4cc7d8cc5ec09bcb0bc1db9d1dc2fc0426a
Enugu Dhanush <dhanushenugu22052006@gmail.com>
2025-08-14 23:36:02 +0530

    Save data and generate HTML for delivery Options

--- data/cart.js
+++ data/cart.js
@@ -5,9 +5,11 @@ if (!cart){
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
     //save the id of the product we want and search for it in product.js this technique is called is normalizing the data
+    deliveryOptionId: '1'
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 // Variables are reset when we go to the different page or reset the page
@@ -34,7 +36,8 @@ export function addToCart(productId){
   } else{
     cart.push({          //if it is not inside insert the item in cart
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'   //for new products we are giving default deliveryOptionId as 1
     });
   }
 
