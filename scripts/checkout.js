36b1edc3dbc769c7aaefd23c87a5223094cc33c2
Enugu Dhanush <dhanushenugu22052006@gmail.com>
2025-08-17 23:25:43 +0530

    Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkout/OrderSummary.js';  
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
-import '../data/cart-class.js'     //Runs all the code inside file without importing
+ // import '../data/cart-class.js'     //Runs all the code inside file without importing
 
 renderOrderSummary();
 renderPaymentSummary();
